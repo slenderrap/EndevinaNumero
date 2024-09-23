703ac806dbc60226357692dd6304f2d649c49d37
slenderrap <slenderrap>
2024-09-23 17:10:07 +0200

    Primers canvis

--- app/src/main/java/com/oriol/endevinanumero/MainActivity.java
+++ app/src/main/java/com/oriol/endevinanumero/MainActivity.java
@@ -1,6 +1,10 @@
 package com.oriol.endevinanumero;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        int num = (int)(Math.random()*100);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        final Button button = findViewById(R.id.numAleatori);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                CharSequence text = String.valueOf(num);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(MainActivity.this , text, duration);
+                toast.show();
+            }
         });
     }
+
+
+
 }
